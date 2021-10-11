b37c20d8286a0898d358c600c903e570f5d456a3
Ernakh <fabriciotonettolondero@gmail.com>
2021-10-10 23:07:50 -0300

    assimétrica implementada

--- CriptografiaSimetricaAndAssimetrica/Program.cs
+++ CriptografiaSimetricaAndAssimetrica/Program.cs
@@ -28,6 +28,15 @@ namespace CriptografiaSimetricaAndAssimetrica
             Console.WriteLine("===============ASSIMÉTRICAS===================");
             Console.WriteLine("============================================");
 
+            Assimetrica assim = new Assimetrica();
+
+            Console.WriteLine("Public Key:" + assim.getPublicKey());
+
+            string cypher = assim.encrypt("Fabrício");
+            Console.WriteLine("Fabrício: " + cypher);
+
+            string texto = assim.decrypt(cypher);
+            Console.WriteLine(texto);
         }
     }
 }
